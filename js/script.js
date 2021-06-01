6c12ff0a3231479c07dac4abbe54644829d10025
Kei <keita_itojpn@yahoo.co.jp>
2021-06-01 21:20:29 +0900

    modify Flocss

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
-const btn = document.querySelector('.c-buttonsidebar');
+const openbtn = document.querySelector('.c-button--menu');
 const sidebar = document.querySelector('.l-sidebar');
-const main = document.querySelector('.c-columnlayout_main');
-btn.addEventListener('click', () => {
+const main = document.querySelector('.c-columnlayout--cover');
+openbtn.addEventListener('click', () => {
     sidebar.classList.toggle('open-menu');
-    main.classList.toggle('close-menu');
+    main.classList.toggle('open-menu');
+});
+const closebtn = document.querySelector('.p-hamburger');
+closebtn.addEventListener('click', () => {
+    sidebar.classList.toggle('open-menu');
+    main.classList.toggle('open-menu');
 });
